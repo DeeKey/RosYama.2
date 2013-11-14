4d82df214c2598f2dab4f042d2e7bb5d67d76b3c
kulandin <admin@rosdostup.ru>
2013-11-14 12:05:37 +0400

    Changes for 1.5 version

---
+++ protected/views/site/pages/contacts.php
@@ -0,0 +1,6 @@
+<?
+$this->pageTitle=Yii::app()->name . ' - Контакты';
+$this->title='Контакты';
+$this->layout='//layouts/header_blank';
+?>
+<iframe height="1100" width="990" frameBorder="3" src="http://rosdostup.ru/dokuwiki_integrated/doc.php?id=contacts:contacts">your browser does not support IFRAMEs</iframe>
---
+++ protected/views/site/pages/workorder.php
@@ -0,0 +1,6 @@
+<?
+$this->pageTitle=Yii::app()->name . ' - Порядок работы';
+$this->title='Порядок работы';
+$this->layout='//layouts/header_blank';
+?>
+<iframe height="1100" width="990" frameBorder="3" src="http://rosdostup.ru/dokuwiki_integrated/doc.php?id=workorder:workorder">your browser does not support IFRAMEs</iframe>
